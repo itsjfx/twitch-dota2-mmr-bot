4014c1351b3bb335b03e0ebac0ba78bf2896fc9e
zypr <itsjfx@users.noreply.github.com>
2016-09-15 01:28:14 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// Configure and rename to "config.js"
+// Create an empty file called "sentry" in same directory
+// Have steamguard code = "" to start
+// Fill in the steamguard code after receiving it
+// After successfully logging in, return steamguard code to ""
+var config = {};
+
+config.steam_name = "";
+config.steam_user = "";
+config.steam_pass = "";
+config.steam_guard_code = "";
+
+module.exports = config;
